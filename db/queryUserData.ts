e8f5e190ddcb109f3576e2ef219a9056e72a5708
Filipe Mendes <filipemendes89@hotmail.com>
2022-06-03 20:51:14 -0300

    <fix> iif indent

--- db/queryUserData.ts
+++ db/queryUserData.ts
@@ -2,8 +2,13 @@ import { IArgumentsRetrieve, IUserDetails } from '../@types/types'
 import { db } from './getDbInstance'
 
 const getQueryParams = (argv: IArgumentsRetrieve) => {
-  const languageQuery = argv.language ? 'additional_info @> \'{ "languages": [{ "name": "$/language:value/" }] }\'' : '1=1'
-  const locationQuery = argv.location ? 'Lower(location) like \'%$/location:value/%\'' : '1=1'
+  const languageQuery = argv.language 
+    ? 'additional_info @> \'{ "languages": [{ "name": "$/language:value/" }] }\'' 
+    : '1=1'
+  
+  const locationQuery = argv.location 
+    ? 'Lower(location) like \'%$/location:value/%\'' 
+    : '1=1'
 
   const params = {
     language: argv.language?.toLowerCase(),
